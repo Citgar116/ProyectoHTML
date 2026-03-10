c7aabb66c6000725556f3c3e48d200c1bf68fa97
Luis Fernando Escobedo Hernandez <lescobedoh2400@alumno.ipn.mx>
2026-03-09 18:23:04 -0600

    Primeras 10 etiquetas/Controlador

---
+++ src/main/java/com/upiiz/proyecto/EtiquetasController.java
@@ -0,0 +1,63 @@
+package com.upiiz.proyecto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EtiquetasController {
+
+    @GetMapping({"/", "/inicio"})
+    public String inicio() {
+        return "inicio";
+    }
+
+    @GetMapping("/html")
+    public String html() {
+        return "html";
+    }
+
+    @GetMapping("/head")
+    public String head() {
+        return "head";
+    }
+
+    @GetMapping("/body")
+    public String body() {
+        return "body";
+    }
+
+    @GetMapping("/h1")
+    public String h1() {
+        return "h1";
+    }
+
+    @GetMapping("/h2")
+    public String h2() {
+        return "h2";
+    }
+
+    @GetMapping("/p")
+    public String p() {
+        return "p";
+    }
+
+    @GetMapping("/span")
+    public String span() {
+        return "span";
+    }
+
+    @GetMapping("/strong")
+    public String strong() {
+        return "strong";
+    }
+
+    @GetMapping("/em")
+    public String em() {
+        return "em";
+    }
+
+    @GetMapping("/a")
+    public String a() {
+        return "a";
+    }
+}
